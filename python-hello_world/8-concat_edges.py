0be0542beee50783df4feef089bb3fb0e7d096a2
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-05-31 08:06:50 -0500

    Adding files

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+separator = str.split()
+print(separator[5]+" "+separator[6]+" "+separator[12]+" "+separator[0])
