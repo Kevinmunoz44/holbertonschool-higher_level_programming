c50b192f36476ee9fc818c6905fe978106f7743c
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-01 14:58:09 -0500

    Update file 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str = ""
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        new_str += i
+    print(new_str)
