fe3048a2306110b88b490612ee4d66145b88c745
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-02 14:59:35 -0500

    Update file 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import sys
-numbers = sys.argv[1:]
-value = sum(int(argm) for argm in numbers)
-print(value)
+if __name__ == "__main__":
+    import sys
+    numbers = sys.argv[1:]
+    value = sum(int(argm) for argm in numbers)
+    print(value)
