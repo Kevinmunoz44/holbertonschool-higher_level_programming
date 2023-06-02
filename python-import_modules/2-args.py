d571bac9ed347367a23953e704e6116a3b3c053d
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-02 14:55:07 -0500

    Update file 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 import sys
-numbers = sys.argv[1:]
-num_len = len(numbers)
+if __name__ == "__main__":
 
-if num_len == 0:
-    print(f"{num_len}", "arguments.")
-elif num_len == 1:
-    print(f"{num_len}", "arguments:")
-else:
-    print(f"{num_len}", "arguments:")
+    numbers = sys.argv[1:]
+    num_len = len(numbers)
 
-for i, number in enumerate(numbers):
-    print(f"{i+1}:", number)
+    if num_len == 0:
+        print(f"{num_len}", "arguments.")
+    elif num_len == 1:
+        print(f"{num_len}", "arguments:")
+    else:
+        print(f"{num_len}", "arguments:")
+
+    for i, number in enumerate(numbers):
+        print(f"{i+1}:", number)
