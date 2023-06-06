9ea9888ea57487c0914b126f19bb1e1723652f26
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-06 10:17:10 -0500

    Update file 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = tuple_a[:2]
+    tuple_b = tuple_b[:2]
     if len(tuple_a) < 2:
         tuple_a += (0,) * (2 - len(tuple_a))
     if len(tuple_b) < 2:
