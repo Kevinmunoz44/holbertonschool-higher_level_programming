cdbe971b5839fddabc17f6bd5440981e81507b45
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-06 13:40:08 -0500

    Update file 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    maxim = my_list[0]
-
+    
     if not my_list:
         return None
+    
+    maxim = my_list[0]
 
     for i in my_list:
         if i > maxim:
