7fb5687f58e1307838e14ef099ea2aba5c8c9f0f
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-07 13:25:36 -0500

    Update file 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    numbers_int = []
-    for element in my_list:
-        if element not in numbers_int:
-            numbers_int.append(element)
-    return numbers_int
+    return sum(set(my_list), 0)
