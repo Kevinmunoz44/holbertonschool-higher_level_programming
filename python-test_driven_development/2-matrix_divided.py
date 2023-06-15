cf31929c4ca189ea108053b520da661c654b35dd
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-15 10:50:43 -0500

    Update files 2-matrix.py and 2-matrix.txt

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,16 +4,19 @@
 
 def matrix_divided(matrix, div):
     
-    '''Conditionally divide'''
+    '''Conditionally wiht type error for matrix and list'''
     if not all(isinstance(row, list) and all(isinstance(num, (int, float)) for num in row) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
+    '''Conditionally wiht type error for matrix'''
     if len(set(len(row) for row in matrix)) != 1:
         raise TypeError("Each row of the matrix must have the same size")
 
+    '''Conditionally wiht type error for div must be a number'''
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
+    '''Conditionally wiht ZeroDivisionError must be a number'''
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
