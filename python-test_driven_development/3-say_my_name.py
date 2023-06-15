9ee0136ecf1f8d8acaac1d25501e7a98a85e2b93
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-15 11:36:40 -0500

    Update file 3-say.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,13 +3,13 @@
 
 
 def say_my_name(first_name, last_name=""):
-    
+
     '''Condition than this converting string'''
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
+
     if last_name:
         print("My name is", first_name, last_name)
     else:
