1c0192dc08bef8a8d12dddcf1b6495cd595b30f0
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-15 10:37:22 -0500

    Update file 2-matrix and change tests the file 2-matrix

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Function than divide a Matirx'''
+
+
+def matrix_divided(matrix, div):
+    
+    '''Conditionally divide'''
+    if not all(isinstance(row, list) and all(isinstance(num, (int, float)) for num in row) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    if len(set(len(row) for row in matrix)) != 1:
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new_matrix = [[round(num / div, 2) for num in row] for row in matrix]
+
+    return new_matrix
