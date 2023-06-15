ce0c3dfca419ca2d36029aba624129715e002842
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-15 11:20:31 -0500

    Update file 2-matrix.py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -3,23 +3,25 @@
 
 
 def matrix_divided(matrix, div):
-    
-    '''Conditionally wiht type error for matrix and list'''
-    if not all(isinstance(row, list) and all(isinstance(num, (int, float)) for num in row) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    """ Check if matrix is a list of lists of integers or floats"""
+    if not all(isinstance(row, list) for row in matrix) or not all(
+        isinstance(num, (int, float)) for row in matrix for num in row
+    ):
+        raise TypeError(
+            ("matrix must be a matrix (list of lists) of integers/floats"))
 
-    '''Conditionally wiht type error for matrix'''
-    if len(set(len(row) for row in matrix)) != 1:
+    """ Check if each row of the matrix has the same size """
+    if len(set(len(row) for row in matrix)) > 1:
         raise TypeError("Each row of the matrix must have the same size")
 
-    '''Conditionally wiht type error for div must be a number'''
+    """ Check if div is a number """
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
-    '''Conditionally wiht ZeroDivisionError must be a number'''
+    """ Check if div is not equal to 0 """
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    new_matrix = [[round(num / div, 2) for num in row] for row in matrix]
-
-    return new_matrix
+    """print new matrix"""
+    matrixNew = [[round(num / div, 2) for num in row] for row in matrix]
+    return matrixNew
