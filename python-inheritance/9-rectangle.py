667c84ed347a4a97f20105ad941c8a5ae290978c
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-21 13:27:32 -0500

    Update file 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Import class BaseGeomety"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+'''Create a class Rectangle'''
+
+
+class Rectangle(BaseGeometry):
+    
+    '''function for privated with and height'''
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        return self.width * self.height
+
+    def __str__(self):
+        return f"[Rectangle] {self.width}/{self.height}"
+    
+    def print(self):
+        print(self)
