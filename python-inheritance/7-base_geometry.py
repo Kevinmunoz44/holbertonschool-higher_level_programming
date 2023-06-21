0b17d4aed81ac117676359a912d77b00c123f491
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-21 11:44:05 -0500

    Update file 7.txt and 7.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """Define class empty"""
+
+
 class BaseGeometry:
     """Class base class for Geometry"""
     def area(self):
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
+
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
