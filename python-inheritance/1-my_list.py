49e9f650f6cb30786c183ea74899ea11d64dd1f2
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-21 08:47:06 -0500

    Update file 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 '''Create a new class call My list'''
 
+
+
 class MyList(list):
     '''Function that prints a list of objects'''
-    
+
     def print_sorted(self):
         listsort = sorted(self)
         print(listsort)
