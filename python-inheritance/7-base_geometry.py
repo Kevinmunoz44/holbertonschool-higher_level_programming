b1f94e4224f2ed5cf0945c166ebf80960d9f95e6
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-21 11:42:52 -0500

    Update file 7.txt and 7.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3
-'''Create a class empty'''
-
-
+"""Define class empty"""
 class BaseGeometry:
-
-    '''define a function init'''
-    def __init__(self):
-        pass
-
-    '''define function for area'''
+    """Class base class for Geometry"""
     def area(self):
         raise Exception("area() is not implemented")
-
-    '''define function for validation of value'''
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
-
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
