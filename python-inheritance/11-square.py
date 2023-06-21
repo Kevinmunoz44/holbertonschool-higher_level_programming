285356934e039356077deb940da8e035fa0189c8
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-21 14:36:25 -0500

    Update file 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Import class Rectangle'''
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+'''Create class call Square'''
+
+
+class Square(Rectangle):
+
+    '''Function for validation of size'''
+    def __init__(self, size):
+        self._size = size
+        self.integer_validator("size", size)
+        '''Super trae todos los metodos de la clase anterior'''
+        super().__init__(size, size)
+
+    '''Function str'''
+    def __str__(self):
+        return f"[Square] {self._size}/{self._size}"
