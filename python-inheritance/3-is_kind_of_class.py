b91901da9931b5a31e31a758b5acd98d11153cd1
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-21 08:44:28 -0500

    Update file 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Same class or inherit from'''
+
+
+def is_kind_of_class(obj, a_class):
+    if isinstance(obj, a_class) or isinstance(obj, a_class):
+        return True
+    else:
+        return False
