c7b07c5a1f1e59b350fe9517abb8d96e59ea9be8
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-21 08:32:03 -0500

    Update file 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -2,9 +2,4 @@
 '''Define a function'''
 
 def is_same_class(obj, a_class):
-
-    '''Validation about if the obj is instance of the class'''
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return type(obj) is a_class
