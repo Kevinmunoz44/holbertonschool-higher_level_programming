5460b0f131ba3611d2d2e87184122729c7aa72d9
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-21 14:27:42 -0500

    Update file 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,15 +2,16 @@
 '''Import class Rectangle'''
 
 
-Rectangle = __import__('9-Rectangle').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 '''Create class call Square'''
 
 
-class Square:
+class Square(Rectangle):
 
     '''Function for validation of size'''
     def __init__(self, size):
         self._size = size
         self.integer_validator("size", size)
+        super().__init__(size, size)
