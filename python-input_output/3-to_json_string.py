bb1c09a699494cf91b109c4381227663369fee33
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-22 10:26:03 -0500

    Update file 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Import module json'''
+import json
+
+
+'''Function that return json'''
+
+
+def to_json_string(my_obj):
+
+    '''return json'''
+    return json.dumps(my_obj)
