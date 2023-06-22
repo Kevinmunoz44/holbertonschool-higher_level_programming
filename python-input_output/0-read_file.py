c2ad89a7a2354a3f87f102410d58e5b6d5df7782
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-22 09:39:48 -0500

    Update file 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-'''Import json'''
-import json
-
-
 '''Define a function that read a file'''
+
+
 def read_file(filename=""):
-    
+
     '''read a file'''
     with open(filename, 'r', encoding='utf-8') as file:
-        content = file.read()
-        print(content, end="")
+        print(file.read(), end="")
