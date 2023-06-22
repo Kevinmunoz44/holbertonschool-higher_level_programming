eb21508508529d1d3521e337d9dbcdfd29c6d8fa
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-22 11:02:14 -0500

    Update file 5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Import module json'''
+import json
+
+
+'''function that writes an Object to a text file'''
+
+
+def save_to_json_file(my_obj, filename):
+
+    with open(filename, "w") as fileobj:
+        fileobj.write(json.dumps(my_obj))
+        return fileobj
