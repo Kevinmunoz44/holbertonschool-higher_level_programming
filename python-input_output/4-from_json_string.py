bb4684230fc4add4fc0f0619266e1ddf6ec63e6a
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-22 10:29:24 -0500

    Update file 4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Import module json'''
+import json
+
+
+'''Function that return json string'''
+
+
+def from_json_string(my_str):
+
+    '''return json string'''
+    return json.loads(my_str)
