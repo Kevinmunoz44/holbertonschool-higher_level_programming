b482f628220e80334a50b43e1c0777dad14e775b
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-26 16:24:39 -0500

    Adding file test_rectangle.py

---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,67 @@
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestBase(unittest.TestCase):
+
+    def test_multiple(self):
+
+        r1 = Rectangle()
+        r2 = Rectangle()
+        self.assertEqual(r1.id, r2.id - 1)
+
+        r3 = Rectangle()
+        self.assertEqual(r1.id, r2.id, r3.id - 2)
+
+        r1 = Rectangle(None)
+        r2 = Rectangle(None)
+        self.assertEqual(r1.id, r2.id - 1)
+
+        self.assertEqual(20, Rectangle().id)
+
+    def test_rectangle_initialization(self):
+        rect = Rectangle(5, 10)
+        self.assertEqual(rect.width, 5)
+        self.assertEqual(rect.height, 10)
+        self.assertEqual(rect.x, 0)
+        self.assertEqual(rect.y, 0)
+        self.assertIsNone(rect.id)
+
+    def test_rectangle_width_validation(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle("invalid", 10)
+        with self.assertRaises(ValueError):
+            rect = Rectangle(0, 10)
+        with self.assertRaises(ValueError):
+            rect = Rectangle(-5, 10)
+
+    def test_rectangle_height_validation(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle(5, "invalid")
+        with self.assertRaises(ValueError):
+            rect = Rectangle(5, 0)
+        with self.assertRaises(ValueError):
+            rect = Rectangle(5, -10)
+
+    def test_rectangle_x_validation(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle(5, 10, "invalid")
+        with self.assertRaises(ValueError):
+            rect = Rectangle(5, 10, -1)
+
+    def test_rectangle_y_validation(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle(5, 10, 0, "invalid")
+        with self.assertRaises(ValueError):
+            rect = Rectangle(5, 10, 0, -1)
+
+    def test_rectangle_area(self):
+        rect = Rectangle(4, 5)
+        self.assertEqual(rect.area(), 20)
+        rect.width = 10
+        rect.height = 3
+        self.assertEqual(rect.area(), 30)
+
+
+if __name__ == '__main__':
+    unittest.main()
