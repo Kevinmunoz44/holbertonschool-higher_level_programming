40814077a2d65a3a3f1a2293963bb7abd4f3ba52
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-26 11:59:03 -0500

    Update file base.py and test_base.py

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,18 @@
+import unittest
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    def test_id_autoincrement(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+
+    def test_id_with_value_start(self):
+        b4 = Base(12)
+        self.assertEqual(b4.id, 12)
+
+if __name__ == '__main__':
+    unittest.main()
