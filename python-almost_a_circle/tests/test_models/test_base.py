5d774858c7b8eba78545d6d9b831b950ad7dc1c0
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-26 12:50:37 -0500

    Update file test_base.py

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -8,7 +8,7 @@ class TestBase(unittest.TestCase):
         self.assertEqual(b1.id,b2.id - 1)
 
         b3 = Base()
-        self.assertEqual(b1.id, b2.id, b3.id - 3)
+        self.assertEqual(b1.id, b2.id, b3.id - 2)
 
         b1 = Base(None)
         b2 = Base(None)
