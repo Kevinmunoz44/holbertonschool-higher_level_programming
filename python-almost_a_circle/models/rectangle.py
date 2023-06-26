860d9cd15bbd05368794a6e7589443f50e754028
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-26 14:24:49 -0500

    Update two files rectangle.py and test_rectangle.py

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+'''Import the class call base'''
+from models.base import Base
+
+
+'''Create class call Rectangle'''
+
+
+class Rectangle(Base):
+
+    '''Define function with constructor'''
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    '''Method get'''
+    @property
+    def width(self):
+        return self.__width
+
+    '''Method set'''
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    '''Method get for width'''
+    @property
+    def height(self):
+        return self.__height
+
+    '''Method set for height'''
+    def height(self, value):
+        self.__height = value
+
+    '''Method get for x'''
+    @property
+    def x(self):
+        return self.__x
+
+    '''Method set for x'''
+    def x(self, value):
+        self.__x = value
+
+    '''Method get for y'''
+    @property
+    def y(self):
+        return self.__y
+
+    '''Method set for y'''
+    def y(self, value):
+        self.__y = value
---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,24 @@
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestBase(unittest.TestCase):
+
+    def test_multiple(self):
+
+        r1 = Rectangle()
+        r2 = Rectangle()
+        self.assertEqual(r1.id, r2.id - 1)
+
+        r3 = Rectangle()
+        self.assertEqual(r1.id, r2.id, r3.id - 2)
+
+        r1 = Rectangle(None)
+        r2 = Rectangle(None)
+        self.assertEqual(r1.id, r2.id - 1)
+
+        self.assertEqual(20, Base().id)
+
+
+if __name__ == '__main__':
+    unittest.main()
