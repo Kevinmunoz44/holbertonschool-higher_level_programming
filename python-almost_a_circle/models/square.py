278dff33a4c836eade158abbf7636b72f2ad8f05
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-27 15:37:47 -0500

    Update file square.py and test_square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,8 +11,19 @@ class Square(Rectangle):
     '''Constructor of the class'''
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-
+        self._size = size
     '''Method str for square'''
     def __str__(self):
         return ("[Square] ({}) {}/{} - {}".format
                 (self.id, self.x, self.y, self.width))
+
+    '''Method get for size'''
+    @property
+    def size(self):
+        return self.width
+
+    '''Method set for size'''
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -12,6 +12,23 @@ class TestSquare(unittest.TestCase):
 
         s = Square(3, 2, 2)
         self.assertEqual(str(s), "[Square] (None) 2/2 - 3")
+        
+    def test_size_getter(self):
+        s = Square(5)
+        self.assertEqual(s.size, 5)
+
+        s.size = 10
+        self.assertEqual(s.size, 10)
+
+    def test_size_setter(self):
+        s = Square(5)
+        s.size = 8
+        self.assertEqual(s.width, 8)
+        self.assertEqual(s.height, 8)
+
+        s.size = 12
+        self.assertEqual(s.width, 12)
+        self.assertEqual(s.height, 12)
 
 if __name__ == '__main__':
     unittest.main()
