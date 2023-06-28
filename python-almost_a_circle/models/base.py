5f36498b5ef758408a0208d6c8225d325401c784
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-28 12:59:57 -0500

    Update the test and base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,17 +1,30 @@
 #!/usr/bin/python3
-'''Class base'''
+"""Define the Base Class"""
 
 
 class Base:
-    '''This is contructor'''
+    """constructor"""
     __nb_objects = 0
 
-    '''Define the function __init__'''
-    def __init__(self, id=None):
+    """Function Initialized"""
 
-        '''Conditionals for id'''
+    def __init__(self, id=None):
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        import json
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        import json
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -4,15 +4,46 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
 
-    def test_multiple(self):
+    def test_base_id_generation(self):
+        base1 = Base()
+        base2 = Base()
+        base3 = Base(89)
+        self.assertEqual(base1.id, 1)
+        self.assertEqual(base2.id, 2)
+        self.assertEqual(base3.id, 89)
 
-        b1 = Base()
-        b2 = Base()
-        self.assertEqual(b1.id, b2.id - 1)
+    def test_base_to_json_string_none(self):
+        json_string = Base.to_json_string(None)
+        self.assertEqual(json_string, "[]")
 
-        b1 = Base(None)
-        b2 = Base(None)
-        self.assertEqual(b1.id, b2.id - 1)
+    def test_base_to_json_string_empty_list(self):
+        json_string = Base.to_json_string([])
+        self.assertEqual(json_string, "[]")
+
+    def test_base_to_json_string_with_data(self):
+        json_string = Base.to_json_string([{'id': 12}])
+        self.assertEqual(json_string, '[{"id": 12}]')
+
+    def test_base_to_json_string_with_data_returning_string(self):
+        json_string = Base.to_json_string([{'id': 12}])
+        self.assertIsInstance(json_string, str)
+
+    def test_base_from_json_string_none(self):
+        result = Base.from_json_string(None)
+        self.assertEqual(result, [])
+
+    def test_base_from_json_string_empty_list(self):
+        result = Base.from_json_string("[]")
+        self.assertEqual(result, [])
+
+    def test_base_from_json_string_with_data(self):
+        result = Base.from_json_string('[{"id": 89}]')
+        self.assertEqual(result, [{'id': 89}])
+
+    def test_base_from_json_string_with_data_returning_list(self):
+        result = Base.from_json_string('[{"id": 89}]')
+        self.assertIsInstance(result, list)
+
 
 if __name__ == '__main__':
     unittest.main()
