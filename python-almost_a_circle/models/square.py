674e3c6bcc1104216ab0dd102e9060b86f2b6957
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-06-28 08:47:25 -0500

    Update files square.py and test_square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -35,8 +35,8 @@ class Square(Rectangle):
         '''Conditional for args'''
         if len(args) > 0:
             self.id = args[0]
-        if len(args):
-            self.size
+        if len(args) > 1:
+            self.size = args[1]
         if len(args) > 2:
             self.x = args[2]
         if len(args) > 3:
